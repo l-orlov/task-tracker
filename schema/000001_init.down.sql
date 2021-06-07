1fc31f088a0af09486a7404e33cf8fda6df6ff54
Lev <l.orlov@arumtrade.com>
2021-06-07 17:25:02 +0300

    Rewrite schema to alternative version

--- schema/000001_init.down.sql
+++ schema/000001_init.down.sql
@@ -1,18 +1,12 @@
 DROP TABLE IF EXISTS
-    r_task,
-    s_project_importance_status,
-    s_project_progress_status,
-    nn_project_user,
-    r_project,
-    r_user
-    CASCADE;
-
-DROP FUNCTION IF EXISTS
-    trigger_insert_default_project_statuses(),
-    trigger_set_r_task_order_num_in_progress_status(),
-    get_project_board(BIGINT),
-    update_project_board_parts(JSONB),
-    update_project_board_progress_statuses(JSONB),
-    update_project_board_progress_status_tasks(JSONB),
-    trigger_set_timestamp(),
+    task_change_log,
+    project_sprint_task,
+    project_sprint,
+    project_task,
+    project_task_progress_status,
+    task_progress_status,
+    task_importance_status,
+    project_user,
+    project,
+    system_user,
     CASCADE;
--- schema/000001_init.up.sql
+++ schema/000001_init.up.sql
@@ -1,269 +1,85 @@
--- naming relied ON this article: http://citforum.ru/database/articles/naming_rule/
-
-CREATE OR REPLACE FUNCTION trigger_set_timestamp()
-    RETURNS TRIGGER
-    LANGUAGE plpgsql
-AS
-$$
-BEGIN
-    NEW.updated_at = NOW();
-    RETURN NEW;
-END;
-$$;
-
--- users
-CREATE TABLE r_user
+CREATE TABLE system_user
 (
-    id                 BIGSERIAL PRIMARY KEY,
-    email              VARCHAR(320) NOT NULL DEFAULT '' UNIQUE,
-    firstname          VARCHAR(255) NOT NULL DEFAULT '',
-    lastname           VARCHAR(255) NOT NULL DEFAULT '',
-    password           VARCHAR(255) NOT NULL DEFAULT '',
-    is_email_confirmed BOOLEAN      NOT NULL DEFAULT FALSE,
-    avatar_url         VARCHAR(500) NOT NULL DEFAULT '',
-    created_at         TIMESTAMPTZ  NOT NULL DEFAULT NOW(),
-    updated_at         TIMESTAMPTZ  NOT NULL DEFAULT NOW()
+    system_user_id BIGSERIAL PRIMARY KEY,
+    firstname      VARCHAR(255) NOT NULL DEFAULT '',
+    lastname       VARCHAR(255) NOT NULL DEFAULT '',
+    email          VARCHAR(320) NOT NULL DEFAULT '' UNIQUE,
+    password       VARCHAR(255) NOT NULL DEFAULT '',
+    created_at     TIMESTAMPTZ  NOT NULL DEFAULT NOW(),
+    updated_at     TIMESTAMPTZ  NOT NULL DEFAULT NOW()
 );
-CREATE TRIGGER set_timestamp
-    BEFORE UPDATE
-    ON r_user
-    FOR EACH ROW
-EXECUTE PROCEDURE trigger_set_timestamp();
 
--- projects to work on
-CREATE TABLE r_project
+CREATE TABLE project
 (
-    id          BIGSERIAL PRIMARY KEY,
+    project_id  BIGSERIAL PRIMARY KEY,
     name        VARCHAR(255) NOT NULL DEFAULT '',
     description TEXT         NOT NULL DEFAULT '',
     created_at  TIMESTAMPTZ  NOT NULL DEFAULT NOW(),
     updated_at  TIMESTAMPTZ  NOT NULL DEFAULT NOW(),
     closed_at   TIMESTAMPTZ  NOT NULL DEFAULT NOW()
 );
-CREATE TRIGGER set_timestamp
-    BEFORE UPDATE
-    ON r_project
-    FOR EACH ROW
-EXECUTE PROCEDURE trigger_set_timestamp();
 
--- importance_statuses for project tasks
-CREATE TABLE s_project_importance_status
+CREATE TABLE project_user
 (
-    id         SERIAL PRIMARY KEY,
-    project_id BIGINT REFERENCES r_project (id) ON DELETE CASCADE,
-    name       VARCHAR(255) NOT NULL DEFAULT '',
-    created_at TIMESTAMPTZ  NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMPTZ  NOT NULL DEFAULT NOW(),
-    UNIQUE (project_id, name)
-);
-CREATE TRIGGER set_timestamp
-    BEFORE UPDATE
-    ON s_project_importance_status
-    FOR EACH ROW
-EXECUTE PROCEDURE trigger_set_timestamp();
-
--- progress_statuses for project tasks
-CREATE TABLE s_project_progress_status
-(
-    id            SERIAL PRIMARY KEY,
-    project_id    BIGINT REFERENCES r_project (id) ON DELETE CASCADE,
-    name          VARCHAR(255) NOT NULL DEFAULT '',
-    order_num     INT          NOT NULL DEFAULT 0,
-    created_at    TIMESTAMPTZ  NOT NULL DEFAULT NOW(),
-    updated_at    TIMESTAMPTZ  NOT NULL DEFAULT NOW(),
-    UNIQUE (project_id, name)
-);
-CREATE TRIGGER set_timestamp
-    BEFORE UPDATE
-    ON s_project_progress_status
-    FOR EACH ROW
-EXECUTE PROCEDURE trigger_set_timestamp();
-
--- insert default statuses for new project
-CREATE OR REPLACE FUNCTION trigger_insert_default_project_statuses()
-    RETURNS TRIGGER
-    LANGUAGE plpgsql
-AS
-$$
-BEGIN
-    INSERT INTO s_project_importance_status (project_id, name)
-    VALUES (NEW.id, 'LOW'),
-           (NEW.id, 'MEDIUM'),
-           (NEW.id, 'HIGH');
-
-    INSERT INTO s_project_progress_status (project_id, name, order_num)
-    VALUES (NEW.id, 'TO DO', 0),
-           (NEW.id, 'IN PROGRESS', 1),
-           (NEW.id, 'DONE', 2);
-
-    RETURN NEW;
-END;
-$$;
-
-CREATE TRIGGER insert_default_project_statuses
-    AFTER INSERT
-    ON r_project
-    FOR EACH ROW
-EXECUTE PROCEDURE trigger_insert_default_project_statuses();
-
--- users working on projects
-CREATE TABLE nn_project_user
-(
-    project_id BIGINT REFERENCES r_project (id) ON DELETE CASCADE NOT NULL,
-    user_id    BIGINT REFERENCES r_user (id) ON DELETE CASCADE    NOT NULL,
-    is_owner   BOOLEAN                                            NOT NULL DEFAULT FALSE,
+    project_user_id BIGSERIAL PRIMARY KEY,
+    project_id      BIGINT REFERENCES project (project_id) ON DELETE CASCADE         NOT NULL,
+    user_id         BIGINT REFERENCES system_user (system_user_id) ON DELETE CASCADE NOT NULL,
+    is_owner        BOOLEAN                                                          NOT NULL DEFAULT FALSE,
     UNIQUE (project_id, user_id)
 );
 
--- tasks to project
-CREATE TABLE r_task
+CREATE TABLE task_importance_status
 (
-    id                           BIGSERIAL PRIMARY KEY,
-    project_id                   BIGINT REFERENCES r_project (id) ON DELETE CASCADE NOT NULL,
-    title                        VARCHAR(255)                                       NOT NULL DEFAULT '',
-    description                  TEXT                                               NOT NULL DEFAULT '',
-    assignee_id                  BIGINT REFERENCES r_user (id)                      NOT NULL,
-    importance_status_id         INT REFERENCES s_project_importance_status (id)    NOT NULL,
-    progress_status_id           INT REFERENCES s_project_progress_status (id)      NOT NULL,
-    order_num_in_progress_status INT                                                NOT NULL DEFAULT 0,
-    created_at                   TIMESTAMPTZ                                        NOT NULL DEFAULT NOW(),
-    updated_at                   TIMESTAMPTZ                                        NOT NULL DEFAULT NOW()
+    task_importance_status_id SERIAL PRIMARY KEY,
+    name                      VARCHAR(255) NOT NULL DEFAULT '' UNIQUE
 );
-CREATE TRIGGER set_timestamp
-    BEFORE UPDATE
-    ON r_task
-    FOR EACH ROW
-EXECUTE PROCEDURE trigger_set_timestamp();
-CREATE INDEX idx_r_task_project_id ON r_task (project_id);
 
-CREATE OR REPLACE FUNCTION trigger_set_r_task_order_num_in_progress_status()
-    RETURNS TRIGGER
-    LANGUAGE plpgsql
-AS
-$$
-DECLARE
-    rec RECORD;
-BEGIN
-    FOR rec IN SELECT *
-               FROM r_task
-               WHERE order_num_in_progress_status >= NEW.order_num_in_progress_status
-                 AND progress_status_id = NEW.progress_status_id
-               ORDER BY order_num_in_progress_status
-        LOOP
-            UPDATE r_task
-            SET order_num_in_progress_status = order_num_in_progress_status + 1
-            WHERE id = rec.id;
-        END LOOP;
+CREATE TABLE task_progress_status
+(
+    task_progress_status_id SERIAL PRIMARY KEY,
+    name                    VARCHAR(255) NOT NULL DEFAULT '' UNIQUE
+);
 
-    RETURN NEW;
-END;
-$$;
+CREATE TABLE project_task_progress_status
+(
+    project_task_progress_status_id SERIAL PRIMARY KEY,
+    task_progress_status_id         BIGINT REFERENCES task_progress_status (task_progress_status_id) ON DELETE CASCADE NOT NULL,
+    project_id                      BIGINT REFERENCES project (project_id) ON DELETE CASCADE                           NOT NULL
+);
 
-CREATE TRIGGER set_r_task_order_num_in_progress_status
-    BEFORE INSERT
-    ON r_task
-    FOR EACH ROW
-EXECUTE PROCEDURE trigger_set_r_task_order_num_in_progress_status();
+CREATE TABLE project_task
+(
+    project_task_id                 BIGSERIAL PRIMARY KEY,
+    title                           VARCHAR(255)                                                                                       NOT NULL DEFAULT '',
+    description                     TEXT                                                                                               NOT NULL DEFAULT '',
+    project_user_id                 BIGINT REFERENCES project_user (project_user_id) ON DELETE CASCADE                                 NOT NULL,
+    task_importance_status_id       BIGINT REFERENCES task_importance_status (task_importance_status_id) ON DELETE CASCADE             NOT NULL,
+    project_task_progress_status_id BIGINT REFERENCES project_task_progress_status (project_task_progress_status_id) ON DELETE CASCADE NOT NULL,
+    created_at                      TIMESTAMPTZ                                                                                        NOT NULL DEFAULT NOW(),
+    updated_at                      TIMESTAMPTZ                                                                                        NOT NULL DEFAULT NOW()
+);
 
--- methods for processing project board json below
-CREATE OR REPLACE FUNCTION get_project_board(_project_id BIGINT)
-    RETURNS JSONB
-    LANGUAGE plpgsql
-AS
-$$
-BEGIN
-    RETURN (SELECT COALESCE(jsonb_agg(
-                                    jsonb_build_object(
-                                            'progressStatusId', spps.id,
-                                            'progressStatusName', spps.name,
-                                            'progressStatusOrderNum', spps.order_num,
-                                            'tasks', COALESCE(t.tasks, '[]'::JSONB)
-                                        )
-                                    ORDER BY (spps.order_num)
-                                ), '[]'::JSONB) board
-            FROM s_project_progress_status spps
-                     LEFT JOIN LATERAL (
-                SELECT rt.progress_status_id,
-                       jsonb_agg(
-                               jsonb_build_object(
-                                       'taskId', rt.id,
-                                       'taskTitle', rt.title,
-                                       'taskOrderNum', rt.order_num_in_progress_status,
-                                       'assigneeId', rt.assignee_id,
-                                       'assigneeFirstname', ru.firstname,
-                                       'assigneeLastname', ru.lastname,
-                                       'assigneeAvatarURL', ru.avatar_url
-                                   )
-                               ORDER BY (rt.order_num_in_progress_status)
-                           ) tasks
-                FROM r_task rt
-                         INNER JOIN r_user ru ON ru.id = rt.assignee_id
-                GROUP BY rt.progress_status_id
-                ) t ON spps.id = t.progress_status_id
-            WHERE spps.project_id = _project_id);
-END;
-$$;
+CREATE TABLE project_sprint
+(
+    project_sprint_id BIGSERIAL PRIMARY KEY,
+    project_id        BIGINT REFERENCES project (project_id) ON DELETE CASCADE NOT NULL,
+    created_at        TIMESTAMPTZ                                              NOT NULL DEFAULT NOW(),
+    updated_at        TIMESTAMPTZ                                              NOT NULL DEFAULT NOW(),
+    closed_at         TIMESTAMPTZ                                              NOT NULL DEFAULT NOW()
+);
 
-CREATE OR REPLACE FUNCTION update_project_board_parts(_board JSONB)
-    RETURNS VOID
-    LANGUAGE plpgsql
-AS
-$$
-DECLARE
-    rec RECORD;
-BEGIN
-    FOR rec IN SELECT board."progressStatusId", tasks."taskId", tasks."taskOrderNum"
-               FROM jsonb_to_recordset(_board) AS board("progressStatusId" INT, "tasks" JSONB)
-                        LEFT JOIN jsonb_to_recordset(board."tasks") AS tasks("taskId" BIGINT, "taskOrderNum" INT)
-                                  ON TRUE
-        LOOP
-            IF rec."progressStatusId" IS NOT NULL AND
-               rec."taskId" IS NOT NULL AND rec."taskOrderNum" IS NOT NULL THEN
-                UPDATE r_task
-                SET progress_status_id           = rec."progressStatusId",
-                    order_num_in_progress_status = rec."taskOrderNum"
-                WHERE id = rec."taskId";
-            END IF;
-        END LOOP;
-END;
-$$;
+CREATE TABLE project_sprint_task
+(
+    task_id           BIGINT REFERENCES project_task (project_task_id) ON DELETE CASCADE     NOT NULL,
+    project_sprint_id BIGINT REFERENCES project_sprint (project_sprint_id) ON DELETE CASCADE NOT NULL,
+    added_at          TIMESTAMPTZ                                                            NOT NULL DEFAULT NOW()
+);
 
-CREATE OR REPLACE FUNCTION update_project_board_progress_statuses(_progress_statuses JSONB)
-    RETURNS VOID
-    LANGUAGE plpgsql
-AS
-$$
-DECLARE
-    rec RECORD;
-BEGIN
-    FOR rec IN SELECT board."progressStatusId", board."progressStatusOrderNum"
-               FROM jsonb_to_recordset(_progress_statuses) AS board("progressStatusId" INT, "progressStatusOrderNum" INT)
-        LOOP
-            IF rec."progressStatusId" IS NOT NULL AND rec."progressStatusOrderNum" IS NOT NULL THEN
-                UPDATE s_project_progress_status
-                SET order_num = rec."progressStatusOrderNum"
-                WHERE id = rec."progressStatusId";
-            END IF;
-        END LOOP;
-END;
-$$;
-
-CREATE OR REPLACE FUNCTION update_project_board_progress_status_tasks(_tasks JSONB)
-    RETURNS VOID
-    LANGUAGE plpgsql
-AS
-$$
-DECLARE
-    rec RECORD;
-BEGIN
-    FOR rec IN SELECT tasks."taskId", tasks."taskOrderNum"
-               FROM jsonb_to_recordset(_tasks) AS tasks("taskId" BIGINT, "taskOrderNum" INT)
-        LOOP
-            IF rec."taskId" IS NOT NULL AND rec."taskOrderNum" IS NOT NULL THEN
-                UPDATE r_task
-                SET order_num_in_progress_status = rec."taskOrderNum"
-                WHERE id = rec."taskId";
-            END IF;
-        END LOOP;
-END;
-$$;
+CREATE TABLE task_change_log
+(
+    task_change_log_id           BIGSERIAL PRIMARY KEY,
+    project_task_id              BIGINT REFERENCES project_task (project_task_id) ON DELETE CASCADE NOT NULL,
+    project_task_progress_status INT                                                                NOT NULL,
+    project_user                 BIGINT                                                             NOT NULL,
+    updated_at                   TIMESTAMPTZ                                                        NOT NULL DEFAULT NOW()
+);
